7afa4a048734b6a4a6aa656d1b9cdab7f1dfad4b
Roger <navyrocklee@gmail.com>
2023-01-31 19:32:50 +0200

    delete dependency

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const { ValidationErrorItem } = require("sequelize");
-
 module.exports = class SequelizeValidationMessages {
     static splitByParams(sequelizeValidationError, ...params) {
-
         let errors = {};
 
-        //generate params object
         for (let param of params) {
             errors[param] = [];
         }
 
         if (sequelizeValidationError.name === 'SequelizeValidationError') {
             for (let error of sequelizeValidationError.errors) {
-                if (error instanceof ValidationErrorItem) {
+                if (error.constructor.name === 'ValidationErrorItem') {
                     errors[error.path].push(error.message);
                 }
             }
